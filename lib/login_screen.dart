86069ec939431dc53cf2fb08677d7f73710469fe
wazir1420 <wazirbalti1@gmail.com>
2024-11-22 02:49:17 +0500

    login_updated

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -15,6 +15,11 @@ class _LoginViewState extends State<LoginView> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passController = TextEditingController();
 
+  // Create FocusNodes for managing focus when typing
+  final FocusNode emailFocusNode = FocusNode();
+  final FocusNode passFocusNode = FocusNode();
+
+  // Handle user login
   login() async {
     try {
       final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
@@ -50,7 +55,6 @@ class _LoginViewState extends State<LoginView> {
           backgroundColor: Colors.red,
         ),
       );
-
       print('FirebaseAuthException: $e');
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -59,166 +63,181 @@ class _LoginViewState extends State<LoginView> {
           backgroundColor: Colors.red,
         ),
       );
-
       print('Error: $e');
     }
   }
 
   @override
+  void dispose() {
+    emailFocusNode.dispose();
+    passFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              height: 250,
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black26,
-                      blurRadius: 10,
-                      offset: Offset(-5, 0),
-                    ),
-                  ],
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30))),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Center(
-                    child: Container(
-                      height: 120,
-                      width: 120,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 10,
-                            offset: Offset(-5, 0),
+      resizeToAvoidBottomInset:
+          true, // Ensures the screen resizes when the keyboard appears
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            // Dismiss keyboard when tapping outside
+            FocusScope.of(context).unfocus();
+          },
+          child: SingleChildScrollView(
+            // Makes the entire page scrollable
+            child: Column(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: 250,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          offset: Offset(-5, 0),
+                        ),
+                      ],
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(30),
+                          bottomRight: Radius.circular(30))),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 50),
+                      Center(
+                        child: Container(
+                          height: 120,
+                          width: 120,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black26,
+                                blurRadius: 10,
+                                offset: Offset(-5, 0),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      child: Center(
-                        child: Image.asset(
-                          'assets/images/plus.png',
-                          fit: BoxFit.cover,
-                          height: 80,
-                          width: 80,
+                          child: Center(
+                            child: Image.asset(
+                              'assets/images/plus.png',
+                              fit: BoxFit.cover,
+                              height: 80,
+                              width: 80,
+                            ),
+                          ),
                         ),
                       ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Quick Medical',
+                        style: TextStyle(
+                            fontSize: 35,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Text(
+                  'Already have an account?',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextFormField(
+                    controller: emailController,
+                    focusNode: emailFocusNode, // Focus node for email field
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      hintText: 'Email Address',
+                      hintStyle: const TextStyle(color: Colors.blue),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30),
+                  child: TextFormField(
+                    controller: passController,
+                    focusNode: passFocusNode, // Focus node for password field
+                    obscureText: _isObscured,
+                    decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      hintText: 'Password',
+                      hintStyle: const TextStyle(color: Colors.blue),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _isObscured ? Icons.visibility_off : Icons.visibility,
+                          color: Colors.blue,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _isObscured = !_isObscured;
+                          });
+                        },
+                      ),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: login,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: const EdgeInsets.symmetric(horizontal: 120),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    'Quick Medical',
-                    style: TextStyle(
-                        fontSize: 35,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold),
+                  child: const Text(
+                    'Sign In',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              'Already have an account?',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 30),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: 'Email Address',
-                  hintStyle: const TextStyle(color: Colors.blue),
                 ),
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: TextFormField(
-                controller: passController,
-                obscureText: _isObscured,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  hintText: 'Password',
-                  hintStyle: const TextStyle(color: Colors.blue),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      _isObscured ? Icons.visibility_off : Icons.visibility,
-                      color: Colors.blue,
+                const SizedBox(height: 20),
+                const Text("Create a new account if you don't have."),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const SignupScreen(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(horizontal: 120),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _isObscured = !_isObscured;
-                      });
-                    },
+                  ),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(color: Colors.blue, fontSize: 20),
                   ),
                 ),
-                keyboardType: TextInputType.visiblePassword,
-              ),
+              ],
             ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: login,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                padding: const EdgeInsets.symmetric(horizontal: 120),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: const Text(
-                'Sign In',
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text("Create a new account if you don't have."),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const SignupScreen(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(horizontal: 120),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: const Text(
-                'Sign Up',
-                style: TextStyle(color: Colors.blue, fontSize: 20),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
